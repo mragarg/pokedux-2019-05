66a1f7e7809c859add33df2cf4b3bba70fce1f3f
Ashish Garg <mragarg@gmail.com>
2019-05-07 13:41:30 -0400

    Adds smartpokelist container

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+
 // ====================================================
 // STATE
 import { createStore, combineReducers } from 'redux';
@@ -150,7 +152,10 @@ window.store = store;
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
